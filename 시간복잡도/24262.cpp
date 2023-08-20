8f22d7c43e50f7c3ebe677be662439dc0d29dbfd
HwangHyeonseok <hhs0995@naver.com>
2023-08-20 15:27:11 +0900

    시간복잡도 O(1) 판별 문제

---
+++ 시간복잡도/24262.cpp
@@ -0,0 +1,19 @@
+// MenOfPassion(A[], n) {
+//     i = ?n / 2?;
+//     return A[i]; # 코드1
+// }
+
+//  코드1의 시간복잡도는 O(1)이다. 즉, n에 상관 없이 무조건 1번 연산
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int count;
+    cin >> count; 
+
+    cout << "1" << "\n";
+    cout << "0";
+}
