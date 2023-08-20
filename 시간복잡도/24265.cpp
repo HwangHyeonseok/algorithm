23abfd41f81ac8026b048b31b53f95b9c849a45f
HwangHyeonseok <hhs0995@naver.com>
2023-08-20 15:53:41 +0900

    시간복잡도 O(n^3) 판별 ㅁ누제

--- 시간복잡도/24265.cpp
+++ 시간복잡도/24265.cpp
@@ -1,14 +1,13 @@
 // MenOfPassion(A[], n) {
 //     sum <- 0;
-//     for i <- 1 to n - 1
-//         for j <- i + 1 to n
-//             sum <- sum + A[i] × A[j]; # 코드1
+//     for i <- 1 to n
+//         for j <- 1 to n
+//             for k <- 1 to n
+//                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
 //     return sum;
 // }
 
-// 코드1 의 시간복잡도는 O(n^2) 이다. 
-// 실행 횟수는 (n-1) + (n-2) + .... + 1 이다.
-// 즉, 등차수열의 합에 의하여 n-1(n) / 2 이다.
+// 코드1 의 시간복잡도는 O(n^3) 이다. 
 
 // 단, n의 최댓값이 50만이므로 overflow 을 대비한다. long long으로 받는다.
 
@@ -20,8 +19,8 @@ int main(void) {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
     long long n;
     cin >> n;
-    cout << n*(n-1)/2 << "\n";
-    cout << "2"; 
+    cout << n*n*n << "\n";
+    cout << "3"; 
 
     return 0;
 }
